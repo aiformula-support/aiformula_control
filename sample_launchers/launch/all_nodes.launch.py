05c9d2b9cdd5ccc97d78c0863d85301c3f85a904
Yuki Akimoto <yuki_akimoto@jp.honda>
2025-07-25 10:05:03 +0900

    Add `sample_launchers`.

---
+++ sample_launchers/launch/all_nodes.launch.py
@@ -0,0 +1,53 @@
+import os.path as osp
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+    launch_args = (
+        DeclareLaunchArgument(
+            "loggger",
+            default_value="info",
+            choices=["debug", "info", "warn", "error", "fatal"],
+            description="Logger level",
+        ),
+    )
+
+    # --- IMU, GNSS --- #
+    vectornav = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            osp.join(get_package_share_directory("sample_launchers"), "launch/vectornav.launch.py"),
+        ),
+    )
+    # --- Publish zero velocity if emergency stop is active --- #
+    emergency_stop_controller = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            osp.join(
+                get_package_share_directory("emergency_stop_controller"), "launch/emergency_stop_controller.launch.py"
+            ),
+        ),
+    )
+    motor_controller = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            osp.join(get_package_share_directory("motor_controller"), "launch/motor_controller.launch.py"),
+        ),
+    )
+    can_receiver_and_sender = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            osp.join(get_package_share_directory("sample_launchers"), "launch/socket_can_bridge.launch.py"),
+        ),
+    )
+
+    return LaunchDescription(
+        [
+            *launch_args,
+            vectornav,
+            emergency_stop_controller,
+            motor_controller,
+            can_receiver_and_sender,
+        ]
+    )
